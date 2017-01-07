abc09a1e368201c1bfa2a8082f6fa5d3ed359ac7
陶梦迪 <taomengdi99@163.com>
2017-01-07 16:42:30 +0800

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170107072238) do
 
   create_table "group_relationships", force: :cascade do |t|
-    t.integer  "group_in"
+    t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
