f99f20208832fe383db316f4247503dc622941bf
陶梦迪 <taomengdi99@163.com>
2017-01-07 15:10:16 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,48 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :only => [:new, :create]
+
+    def new
+      @group = Group.find(params[:group_id])
+      @post = Post.new
+    end
+
+    def create
+      @group = Group.find(params[:group_id])
+      @post = Post.new(post_params)
+      @post.group = @group
+      @post.user = current_user
+      if @post.save
+        redirect_to group_path(@group)
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to account_posts_path, alert: 'Post deleted!'
+    end
+
+    def edit
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post =  Post.find(params[:id])
+      if @post.updata(post_params)
+        redirect_to account_posts_path, notice: 'Update Success'
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:content)
+
+    end
+
 end
